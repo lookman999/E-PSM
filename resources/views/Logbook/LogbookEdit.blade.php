a11855498d771350bf438ff45bf94e976971c922
FatinSyahidah <fatinsyahidah75@gmail.com>
2023-01-14 17:44:21 +0800

    Student edit logbook

--- resources/views/Logbook/LogbookEdit.blade.php
+++ resources/views/Logbook/LogbookEdit.blade.php
@@ -34,17 +34,19 @@
             <h2><br></h2>
 
 
-            <form action = "/editlogbook/" method = "post" class="form-group" style="width:70%; margin-left:15%;" action="/action_page.php">
-              <input type = "hidden" name = "_token" value = "">
+            <form action = "/editlogbook/<?php echo $users[0]->id; ?>" method = "post" class="form-group" style="width:70%; margin-left:15%;">
+              <input type = "hidden" name = "_token" value = "<?php echo csrf_token(); ?>">
 
                   <label for="Title">Title   :</label><br>
-                  <input type="text" id="Title" name="Title" style="width:500px;"><br><br>
+                  <input type="text" id="Title" name="Title" value='<?php echo $users[0]->Title; ?>' style="width:500px;"><br><br>
+
                   <label for="name">Date :</label><br>
-                  <input type="text" id="Date" name="Date" style="width:500px;"><br><br>
+                  <input type="text" id="Date" name="Date" value='<?php echo $users[0]->Date; ?>' style="width:500px;"><br><br>
                   <label for="Prepared_by">Prepared By :</label><br>
-                  <input type="text" id="Prepared_by" name="Prepared_by" style="width:500px;"><br><br>
+                  <input type="text" id="Prepared_by" name="Prepared_by" value='<?php echo $users[0]->Prepared_by; ?>'style="width:500px;">
+                  <br><br>
                   <label for="Description">Description :</label><br>
-                  <textarea name="Description" rows="10" cols="80"></textarea>
+                  <textarea name="Description" value='<?php echo $users[0]->Description; ?>' rows="10" cols="80"></textarea>
                   <br><br>
                   <h2><br></h2>
                   <button type="submit"  value = "Edit " class="button-73" >Save Logbook</button>
