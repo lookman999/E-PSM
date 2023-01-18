1fb9e0d7f632934930b8fe5b53979547ca9a4f56
kha144 <102439604+kha144@users.noreply.github.com>
2023-01-18 15:15:03 +0530

    Update Addsv.blade.php
    Add Supervisor blade file modification

--- resources/views/SvHunting/Addsv.blade.php
+++ resources/views/SvHunting/Addsv.blade.php
@@ -1,27 +1,40 @@
-//this for coordinator add sv
+{{-- this for coordinator add sv --}}
 <?php
 
 $logged_user = session()->get('logged_user');
 $user_name = session()->get('name');
 $roles = session()->get('user_type');
 
-// $all = session()->all();
-
-// var_dump($all);
 ?>
 
 <x-header-new />
 
 <style>
-	
+
 	table.center{
 		margin-left :auto;
 		margin-right :auto;
 	}
-	
+    .input{
+        width:500px;
+        padding:8px;
+    }
+
+
 </style>
 
 <!-- Content Wrapper. Contains page content -->
+<link href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.0.0-
+alpha/css/bootstrap.css" rel="stylesheet">
+
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+
+<link rel="stylesheet" type="text/css"
+href="https://cdnjs.cloudflare.com/ajax/libs/toastr.js/latest/toastr.min.css">
+
+<script src="https://cdnjs.cloudflare.com/ajax/libs/toastr.js/latest/js/toastr.min.js"></script>
+
+
 <div class="content-wrapper">
 
   <!-- Content Header (Page header) -->
@@ -48,35 +61,54 @@ $roles = session()->get('user_type');
       <table style="width:100%">
         <tr>
          	<center>
-			 <h1 style="text-align: center;">ADD SV FORM</h1>
-        </br>
-		
-   	<button><a href="Addsv" >Add Supervisor<class="nav-link  @if(url()->current() ===  URL::to('/Addsv'))
-		active
-		@endif"></class="nav-link></a></button>
-		<button><a href="ViewSVListCo" >View Supervisor List<class="nav-link  @if(url()->current() ===  URL::to('/ViewSVListCo'))
-		active
-		@endif"></class="nav-link></a></button>
+                <h1 style="text-align: center; background-color:rgb(232, 199, 199); color:black; margin:15px; padding:12px; display:inline-block;  border-radius: 35px;" >ADD SV FORM</h1>
 
-		<button <a href="" target="_blank">Delete Supervisor </a></button>
+               <script>
+                @if(Session::has('success'))
+                toastr.options =
+                {
+                    "closeButton" : true,
+                    "progressBar" : true
+                }
+                        toastr.success("{{ session('success') }}");
+                @endif
+                </script>
+
+
+                @if (Session::has('error'))
+                <div class="alert alert-danger">
+                    {{ Session::get('error') }}
+                </div>
+                @endif
+
+
+        </br>
+
+        <a href="{{ route('AddrecordSV') }}" class="btn btn-md btn-success ">Add Supervisor<class="nav-link  @if(url()->current() ===  URL::to('/ApplySV'))
+            active
+            @endif"></class="nav-link></a>
+
+        <a href="ViewSVList" class="btn btn-md btn-dark">View Supervisor List<class="nav-link  @if(url()->current() ===  URL::to('/ViewSVList')) active	@endif"></class="nav-link></a>
+
+        {{-- <a href="" target="_blank" class="btn btn-md btn-danger">Delete Supervisor List </a> --}}
 
   </center>
 
   <center><fieldset>
          <br>
 
-       <table class="center">
+       <table class="table table-hover">
         <form action="Addsv" method="POST" >
                 @csrf
         <center>
-        
+
         <td><td>
           <p>
           <td>
           <label for=Name>Name:</label>
           </td>
           <td>
-          <input type="Name" name="Name">
+          <input type="Name" name="Name" class="input" required>
           </td>
           </p>
         </td>
@@ -88,7 +120,7 @@ $roles = session()->get('user_type');
           <label for=Supervisor_ID>Supervisor ID:</label>
           </td>
           <td>
-          <input type="Supervisor_ID" name="Supervisor_ID">
+          <input type="Supervisor_ID" name="Supervisor_ID" class="input" required>
           </td>
           </p>
         </td>
@@ -100,7 +132,7 @@ $roles = session()->get('user_type');
           <label for=Faculty>Faculty:</label>
           </td>
           <td>
-          <input type="Faculty" name="Faculty">
+          <input type="Faculty" name="Faculty" class="input" required>
           </td>
           </p>
         </td>
@@ -112,7 +144,7 @@ $roles = session()->get('user_type');
           <label for="Expertise">Expertise:</label>
           </td>
           <td>
-          <input type="text" name="Expertise"> 
+          <input type="text" name="Expertise" class="input" required>
           </td>
           </p>
         </td>
@@ -124,7 +156,7 @@ $roles = session()->get('user_type');
           <label for="Office">Office:</label>
           </td>
           <td>
-          <input type="text" name="Office"> 
+          <input type="text" name="Office" class="input" required>
           </td>
           </p>
         </td>
@@ -136,7 +168,7 @@ $roles = session()->get('user_type');
           <label for="Phon_Number">Phone Number:</label>
           </td>
           <td>
-          <input type="text" name="Phone_Number"> 
+          <input type="text" name="Phone_Number" class="input" required>
           </td>
           </p>
         </td>
@@ -148,7 +180,7 @@ $roles = session()->get('user_type');
           <label for="Email">Email:</label>
           </td>
           <td>
-          <input type="text" name="Email"> 
+          <input type="text" name="Email" class="input" required>
           </td>
           </p>
         </td>
@@ -157,7 +189,8 @@ $roles = session()->get('user_type');
       </table>
       <br>
       <center>
-      <input type="submit" name="Submit" id="Submit" value="Submit" method="post">
+      <button name="Submit" class="btn btn-lg btn-primary" id="Submit">Submit</button>
+      <button name="Reset" class="btn btn-lg btn-danger" id="Submit" value="reset">Clear</button>
             </center>
       </p>
       </form>
